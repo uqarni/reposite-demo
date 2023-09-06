c0b45110bc4a1a5da0af6aade131510c53e35b0b
uqarni <108438834+uqarni@users.noreply.github.com>
2023-09-06 17:47:55 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -27,47 +27,47 @@ def ideator(messages):
         if i < 4:  # we don't want to wait after the last try
           time.sleep(5)  # wait for 5 seconds before the next attempt
   
-  def split_sms(message):
-      import re
+    def split_sms(message):
+        import re
   
-      # Use regular expressions to split the string at ., !, or ? followed by a space or newline
-      sentences = re.split('(?<=[.!?]) (?=\\S)|(?<=[.!?])\n', message.strip())
-      # Strip leading and trailing whitespace from each sentence
-      sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+        # Use regular expressions to split the string at ., !, or ? followed by a space or newline
+        sentences = re.split('(?<=[.!?]) (?=\\S)|(?<=[.!?])\n', message.strip())
+        # Strip leading and trailing whitespace from each sentence
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
   
-      # Compute the cumulative length of all sentences
-      cum_length = [0]
-      for sentence in sentences:
-          cum_length.append(cum_length[-1] + len(sentence))
+        # Compute the cumulative length of all sentences
+        cum_length = [0]
+        for sentence in sentences:
+            cum_length.append(cum_length[-1] + len(sentence))
       
-      total_length = cum_length[-1]
+            total_length = cum_length[-1]
   
-      # Find the splitting point
-      split_point = next(i for i, cum_len in enumerate(cum_length) if cum_len >= total_length / 2)
+        # Find the splitting point
+        split_point = next(i for i, cum_len in enumerate(cum_length) if cum_len >= total_length / 2)
   
-      # Split the sentences into two parts at the splitting point
-      part1 = sentences[:split_point]
-      part2 = sentences[split_point:]
+        # Split the sentences into two parts at the splitting point
+        part1 = sentences[:split_point]
+        part2 = sentences[split_point:]
   
-      # Join the sentences in each part back into strings and exclude any part that is empty
-      strings = []
-      if part1:
-          strings.append(" ".join(part1))
-      if part2:
-          strings.append(" ".join(part2))
+        # Join the sentences in each part back into strings and exclude any part that is empty
+        strings = []
+        if part1:
+            strings.append(" ".join(part1))
+        if part2:
+            strings.append(" ".join(part2))
       
-      return strings
+        return strings
 
-  split_response = [response]
-  count = len(split_response)
-  for section in split_response:
-    section = {
-      "role": "assistant", 
-      "content": section
-    }
-    messages.append(section)
+    split_response = [response]
+    count = len(split_response)
+    for section in split_response:
+        section = {
+           "role": "assistant", 
+           "content": section
+           }
+        messages.append(section)
 
-  return messages, count
+    return messages, count
 
 
 
